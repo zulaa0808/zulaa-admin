8bc3d14c257711d7e1417dab8b1a489c51abb40b
Enkhzul <enh9575@gmail.com>
2023-09-14 10:15:33 +0800

    add update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const removeImports = require('next-remove-imports')();
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = removeImports({
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+      },
+     
+    ],
+  },
+});
+
+module.exports = nextConfig;
